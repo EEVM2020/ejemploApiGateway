259cf527bd28ca9df2f62cc294faf6f5ebef0712
DESKTOP-70PH6G6\DELL <eslimesaDev@gmail.com>
2026-03-07 01:12:57 -0500

    prueba

--- msPedidos/src/main/java/com/msPedidos/controller/PedidosController.java
+++ msPedidos/src/main/java/com/msPedidos/controller/PedidosController.java
@@ -32,6 +32,8 @@ public class PedidosController {
 		pedidos.add(pedido);
 		pedido= new PedidoDto(5L,"Cuaderno",2);
 		pedidos.add(pedido);
+		pedido= new PedidoDto(6L,"Pc",34);
+		pedidos.add(pedido);
 		Map<String, Object> response= new HashMap<>();
 		response.put("pedidos", pedidos);
 		
