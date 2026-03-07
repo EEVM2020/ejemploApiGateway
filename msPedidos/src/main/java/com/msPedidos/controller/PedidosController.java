2695e1c0224912f96dcbf287b74657550661c9f7
DESKTOP-70PH6G6\DELL <eslimesaDev@gmail.com>
2026-03-06 21:53:18 -0500

    se adiciona producto prueba argo

--- msPedidos/src/main/java/com/msPedidos/controller/PedidosController.java
+++ msPedidos/src/main/java/com/msPedidos/controller/PedidosController.java
@@ -28,6 +28,8 @@ public class PedidosController {
 		pedidos.add(pedido);
 		pedido= new PedidoDto(3L,"Camas",32);
 		pedidos.add(pedido);
+		pedido= new PedidoDto(4L,"Colchón",2);
+		pedidos.add(pedido);
 		Map<String, Object> response= new HashMap<>();
 		response.put("pedidos", pedidos);
 		
